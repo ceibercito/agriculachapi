42368054dbfa1e7099e48c10e614841832bdf2f5
CeiberGaribay <ceiber.garibay@materiagris.pe>
2023-03-28 18:46:49 -0500

    primeros cambios agricola chapi

---
+++ src/common/NavBar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {Box, AppBar, Toolbar, Container, Grid, Button, Typography, Stack} from "@mui/material";
+
+export const NavBar: React.FC<{}> = () => {
+    return(
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position="fixed">
+                <Toolbar>
+                    <Container maxWidth="xl">
+                        <Grid container direction={"row"} justifyContent="space-between" alignItems="center">
+                            <Grid item>
+                                <Typography>Agricola Chapi</Typography>
+                            </Grid>
+                            <Grid item>
+                                <Stack direction="row" spacing={2}>
+                                <Button variant="contained">Login</Button>        
+                                <Button variant="outlined">Register</Button>
+                                </Stack>
+                                      
+                                
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+} 
