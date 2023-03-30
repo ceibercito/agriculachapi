6a3e1df4e3dd04732e222dd8efe70ba35471311b
CeiberGaribay <ceiber.garibay@materiagris.pe>
2023-03-30 14:20:35 -0500

    asignacion de obtencion de ultimos cambios en produccion

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 echo "Swithching to branch main"
 git checkout main
 
+echo "Obteniendo ultimos cambios"
+git pull origin main
+
 echo "BUilding app"
 npm run build
 
 echo "Deploying files to server"
-rsync -avP build/ root@chapidigital.com.pe:/var/example.com/
+rsync -avP build/ root@chapidigital.com.pe:/var/www/chapidigital.com.pe/
 echo "Deployment complete"
